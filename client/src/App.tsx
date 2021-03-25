f766f0fd6ed9361d08a453e20152b39c328bbd06
Stephen Wan <stephen@stephenwan.com>
2021-03-25 14:29:08 -0700

    client: add wanakani input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import * as wanakana from "wanakana";
 
 function App() {
   return (
@@ -17,10 +18,41 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <WanakanaInput />
         </a>
       </header>
     </div>
   );
+}
+
+function WanakanaInput() {
+  const inputRef = useRef(null as null | HTMLInputElement);
+  useWanakana(inputRef, {
+    IMEMode: true,
+  });
+  return <input autoFocus ref={inputRef} />;
+}
+
+/**
+ * useWanakana is a React hook that binds the input ref to wanakana with the given options.
+ * @param ref
+ * @param options
+ */
+function useWanakana(
+  ref: React.MutableRefObject<HTMLInputElement | HTMLTextAreaElement | null>,
+  options?: wanakana.WanakanaOptions
+) {
+  useEffect(() => {
+    const input = ref.current;
+    if (!input) {
+      return;
+    }
+
+    wanakana.bind(input, options);
+    return () => {
+      wanakana.unbind(input);
+    };
+  }, [ref, options]);
 }
 
 export default App;
