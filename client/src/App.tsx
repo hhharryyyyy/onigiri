9b18ac784e0cc3092d8e942b27cdc64f355a2f5e
Stephen Wan <stephen@stephenwan.com>
2021-03-25 14:44:00 -0700

    client: remove some junk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import * as wanakana from "wanakana";
 
@@ -7,19 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <WanakanaInput />
-        </a>
+        <WanakanaInput />
       </header>
     </div>
   );
